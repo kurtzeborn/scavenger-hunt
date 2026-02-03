e3f86e529d5b57f978073a848abe9293328aa205
Scott Kurtzeborn <scott@kurtzeborn.org>
2026-02-03 12:48:55 -0800

    Simplify deployment - let SWA build functions

--- functions/src/index.ts
+++
@@ -1,7 +0,0 @@
-// Entry point for Azure Functions v4 - imports all function modules
-import './functions/gamekeepers.js';
-import './functions/games.js';
-import './functions/me.js';
-import './functions/media.js';
-import './functions/scenarios.js';
-import './functions/teams.js';
